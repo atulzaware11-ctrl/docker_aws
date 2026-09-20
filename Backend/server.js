228b7a1383b5ebf00691cceccbbfe3ae01cc6ca8
atulzaware11-ctrl <atulzaware11@gmail.com>
2026-09-20 20:04:23 +0530

    Refactor server.js to update static file serving

--- Backend/server.js
+++ Backend/server.js
@@ -1,42 +1,31 @@
 import express from "express"
-import { createServer, get } from "http"
+import { createServer } from "http"
 import { Server } from "socket.io"
 import { YSocketIO } from "y-socket.io/dist/server"
-import { fileURLToPath } from "url"
-import { dirname } from "path"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
-// Create an Express application
-// This will be used to handle HTTP requests and serve any necessary files
 const app = express()
-app.use(express.static("../Frontend/dist"))
+app.use(express.static("public"))
+
+
 const httpServer = createServer(app)
 
-const io = new Server(httpServer,{ // io === ??
-    cors:{
-        origin:"*",
-        methods:["GET","POST"]
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: [ "GET", "POST" ]
     }
-}
+})
 
-)
-// 
 
 const ySocketIO = new YSocketIO(io)
-ySocketIO.initialize() 
-// this is for the 
+ySocketIO.initialize()
 
 
-
-app.use((req, res) => {
-    res.sendFile("index.html", { root: __dirname + "/../Frontend/dist" })
-})
-app.get('/health',(req, res)=>{
+app.get('/health', (req, res) => {
     res.status(200).json({
-        message:"ok",
-        success:true
+        message: "ok",
+        success: true
     })
 })
 
